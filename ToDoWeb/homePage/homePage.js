8c0e373821ca18b634070758903150283afa0aa4
Renz Viloria <viloriarenz917@gmail.com>
2025-04-24 00:10:40 +0800

    just finished the js functionality for add, delete, and check task

--- ToDoWeb/homePage/homePage.js
+++ ToDoWeb/homePage/homePage.js
@@ -1,8 +1,9 @@
 const openTasksButton = document.querySelectorAll('[data-tasks-target]')
 const closeTasksButton = document.querySelectorAll('[data-close-button]')
-// const addTask = document.getElementById('intTasks')
 const taskList = document.getElementById('taskList')
 const overlay = document.getElementById('overlay')
+const addTask = document.getElementById("intTasks")
+const taskLists = document.getElementById("taskList")
 
 openTasksButton.forEach(button => {
     button.addEventListener('click', () => {
@@ -18,20 +19,6 @@ closeTasksButton.forEach(button =>{
     })
 })
 
-// addTask.addEventListener('keypress', function(event){
-//     if (event.key === 'Enter') {
-//         event.preventDefault();
-//         document.getElementById('taskList').click();
-//     }
-// })
-
-// // function addTasks() {
-// //     if (inputBox.value === '') {
-// //         alert("You must right something")
-// //     }
-// // }
-
-
 function openTasks(tasks) {
     if (tasks == null) return
     tasks.classList.add('active')
@@ -43,3 +30,32 @@ function closeTasks(tasks) {
     tasks.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+addTask.addEventListener('keypress', function(event) {
+    if(event.key === "Enter") {
+        event.preventDefault();
+
+        const inputTask = addTask.value.trim();
+
+        if (inputTask !== "") {
+            let li = document.createElement('li');
+            li.innerHTML = inputTask;
+            taskLists.appendChild(li);
+            let span = document.createElement('span'); 
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+            addTask.value = "";
+        } else {
+            alert("Write something");
+        }
+        
+    }
+})
+
+taskLists.addEventListener('click', function(e) {
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked')
+    } else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+})
